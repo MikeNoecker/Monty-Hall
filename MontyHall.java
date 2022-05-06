edf717100d4e06aaa59b810d4b9e0d2c25190f6f
M4V_M4N <93604070+MikeNoecker@users.noreply.github.com>
2022-05-06 12:10:20 -0400

    Add files via upload

---
+++ MontyHall.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class MontyHall
+{
+	public static void main (String[] args)
+		{
+			//vars
+			int guess;
+			int correctDoor;
+
+			Scanner keyboard = new Scanner(System.in); // check this later-----
+			Random rand = new Random();
+
+
+			//user input
+			System.out.print("Enter a door number (Door 1, Door 2, or Door 3): ")
+	}
+}
